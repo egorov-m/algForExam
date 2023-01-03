0e6ce8999f59a0ac3fd5336197315365145731b4
Egorov Mikhail <m-5321e@yandex.com>
2023-01-04 00:32:19 +0500

    Added BubbleSort code

---
+++ algForExam/BubbleSortExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace algForExam
+{
+    public static class BubbleSortExtensions
+    {
+        public static IList<T> BubbleSort<T>(this IList<T> collection) where T : IComparable
+        {
+            for (var i = 0; i < collection.Count; i++)
+            {
+                for (var j = i + 1; j < collection.Count; j++)
+                {
+                    if (collection[i].CompareTo(collection[j]) > 0) // Сортировка по возрастанию
+                    {
+                        (collection[j], collection[i]) = (collection[i], collection[j]);
+                    }
+                }
+            }
+
+            return collection;
+        }
+
+        public static IList<T> BubbleSort<T>(this IList<T> collection, IComparer<T> comparer) where T : IComparable
+        {
+            for (var i = 0; i < collection.Count; i++)
+            {
+                for (var j = i + 1; j < collection.Count; j++)
+                {
+                    if (comparer.Compare(collection[i], collection[j]) > 0)
+                    {
+                        (collection[j], collection[i]) = (collection[i], collection[j]);
+                    }
+                }
+            }
+
+            return collection;
+        }
+    }
+}
